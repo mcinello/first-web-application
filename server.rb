ea5788836b2cbce20083aab2820b9bd608f8ba5b
Michelle Cinello <m.cinello@gmail.com>
2017-10-04 12:32:41 -0400

    added page on favourite websites

--- server.rb
+++ server.rb
@@ -10,8 +10,12 @@ get '/portfolio' do
 end
 
 get '/about_me' do
-
   @skills = ['git', 'HTML', 'CSS', 'Ruby']
   @interests = ['Rick and Morty', 'skincare', 'coffee', 'dogs']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['www.beautybloggers.ca', 'www.netflix.com', 'www.crunchyroll.com']
+  erb :favourites
 end
